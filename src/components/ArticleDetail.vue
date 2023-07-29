2e2b3479a838b85a9a230dfd1259487e172e221e
Ozodbek Jumaev <ozodjon6@gmail.com>
2023-07-29 15:56:14 +0500

    #49 Article Detail UI

--- src/components/ArticleDetail.vue
+++ src/components/ArticleDetail.vue
@@ -1,6 +1,36 @@
 <template>
-  <h1>Article Detail</h1>
-  {{article}}
+  <div class="row">
+    <div class="col-12">
+      <h2>{{article?.title}}</h2>
+    </div>
+    <div class="col-12 mt-3">
+      <p class="opacity-75">Description:</p>
+      <p class="font-monospace">{{article?.description}}</p>
+    </div>
+    <div class="row">
+      <div class="col-4 my-4">
+        <div class="d-flex align-items-center gap-3">
+          <div class="d-flex align-items-center justify-content-center bg-primary"
+               style="width: 60px; height: 60px; border-radius: 100%"
+          >
+            <span class="text-light text-uppercase fw-bold display-6">{{article?.author.username[0]}}</span>
+          </div>
+          <p class="f-bold">{{article?.author.username}}</p>
+        </div>
+      </div>
+      <div class="col-2 d-flex align-items-center my-4">
+        <p class="opacity-75 d-block" style="margin-right: 12px">Created at:</p>
+        <p class="font-monospace">{{new Date(article?.createdAt).toLocaleDateString('us')}}</p>
+      </div>
+      <div class="col-2 d-flex align-items-center my-4">
+        <p class="opacity-75 d-block" style="margin-right: 12px">Updated at:</p>
+        <p class="font-monospace">{{new Date(article?.updatedAt).toLocaleDateString('us')}}</p>
+      </div>
+    </div>
+    <div class="col-12 mt-3">
+      <p class="font-monospace">{{article?.body}}</p>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -11,6 +41,6 @@ export default {
     ...mapState({
       article: state => state.articles.articleDetail
     })
-  }
+  },
 }
 </script>
